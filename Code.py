036c9ff81e4af2f720ff57d6d619f7a1259d4f28
Suraj Kale <kalesuraj37193@gmail.com>
2021-12-06 17:58:06 +0530

    Attached traditional method and button in gui

--- Code.py
+++ Code.py
@@ -2,18 +2,41 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 # try:
-from PIL import Image 
+from PIL import Image
 # except ImportError:
 #     import Image
 from imutils import paths
 import argparse
 from scipy import ndimage
 
-def isGreyScale(image):
+s_thr = 0.5
+
+# new file name for saving image
+
+
+def getNewFileNameForEnhancedImage(file_name, prefix):
+    split_file_name = file_name.split('/')
+    print(split_file_name)
+
+    split_file_name[-1] = prefix + split_file_name[-1]
+    new_file_name = "/".join(split_file_name) 
+
+    # print("new_file_name ", new_file_name)
+
+    return new_file_name
+
+# testing getNewFileNameForEnhancedImage()
+# a = getNewFileNameForEnhancedImage("E:/MLCollegeProject/Anup/taj.jpeg", "asdfs_")
+# print(a)
+
+def isGreyScale(file_name):
+    image = cv2.imread(file_name)
+    print("len(image.shape)", len(image.shape), "\n")
     if(len(image.shape) < 3):
         return True
     elif len(image.shape) == 3:
         return False
+
 
 def isNoisy(image):
 
@@ -38,14 +61,14 @@ def isNoisy(image):
     # print("Greyscale image")
     print("s_perc \n\n", s_perc)
 
-    s_thr = 0.5
-    return s_perc > s_thr 
+    return s_perc
 
 
 def variance_of_laplacian(image):
     # compute the Laplacian of the image and then return the focus
     # measure, which is simply the variance of the Laplacian
     return cv2.Laplacian(image, cv2.CV_64F).var()
+
 
 def isBlurry(image):
     # ap = argparse.ArgumentParser()
@@ -56,26 +79,26 @@ def isBlurry(image):
     # args = vars(ap.parse_args())
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     fm = variance_of_laplacian(gray)
-    
-    print("fm \n\n", fm)
-    if fm < 100 :
-        return True
-    return False
 
-def enhanceColorImage(file_name) :
-    print(file_name)
+    print("fm \n\n", fm)
+    return fm
+
+
+def enhanceColorImage(file_name):
+    print("started enhanceColorImage() ")
+
     image = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)
 
     print(len(image.shape))
-    
+
     print("Color Image")
-    
-    if isNoisy(image) == True:
+
+    if isNoisy(image) > s_thr:
         image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
-    if isBlurry(image) == True:
+    if isBlurry(image) < 100:
         sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
         image = cv2.filter2D(image, -1, sharpen_kernel)
-    
+
     lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 
     lab_planes = cv2.split(lab)
@@ -88,29 +111,53 @@ def enhanceColorImage(file_name) :
     lab = cv2.merge(lab_planes)
 
     bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
-    
-    # new file name for saving image
-    split_file_name = file_name.split('/')
-    split_file_name[-1] = "enhanced_" + split_file_name[-1] 
-    new_file_name = "/".join(split_file_name)
-    
+
+    new_file_name = getNewFileNameForEnhancedImage(file_name, "enhanced_")
+
     cv2.imwrite(new_file_name, image)
-    
+
+    print("stopped enhanceColorImage()\n")
+
     return new_file_name
 
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 
 
-def enhanceGreyscaleImage(image):
+def enhanceGreyscaleImage(file_name):
+    image = cv2.imread(file_name)
+
     clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
     cl_img = clahe.apply(image)
     # plt.hist(cl_img.flat, bins=100, range=(100, 255))
     ret, thresh = cv2.threshold(
         cl_img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    cv2.imshow("image.jpg", thresh)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+    new_file_name = getNewFileNameForEnhancedImage(file_name, "greyscale_")
+
+    cv2.imwrite(new_file_name, cl_img)
+
+
+def traditionalMethod(file_name):
+    print("started traditionalMethod()")
+
+    img = cv2.imread(file_name)
+
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+
+    # equalize the histogram of the Y channel
+    img_yuv[:, :, 0] = cv2.equalizeHist(img_yuv[:, :, 0])
+
+    # convert the YUV image back to RGB format
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+
+    new_file_name = getNewFileNameForEnhancedImage(file_name, "traditional_")
+
+    cv2.imwrite(new_file_name, img_output)
+
+    print("stopped traditionalMethod()")
+    
+    return new_file_name
 
 
 # if isGreyScale(image) == True:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import the required Libraries
+import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 from tkinter.filedialog import askopenfile
@@ -12,51 +13,16 @@ import PIL.ImageTk
 
 # import ShowImage
 
-# Create an instance of tkinter frame
-root = tk.Tk()
-
-# Give title to the tkinter frame
-root.title('Image Enhancement')
-
-# Create canvas
-canvas = tk.Canvas(root, bg='#DDA0DD', height=10000, width=10000)
-canvas.pack()
-
-# background_image = tk.PhotoImage('Black')
-# background_label = tk.Label(root, image=background_image)
-# background_label.place(relwidth=1, relheight=1)
-
-# Create different frames into the canvas
-# Our model
-main_frame = tk.Frame(root, bg='#595959', bd=10)
-# Set the position of frame
-main_frame.place(relx=0.5, rely=0, relwidth=0.90, relheight=0.1, anchor='n')
-# Give label for the frame
-model = tk.Label(main_frame, text="Our Model",
-                 bg="white", font=('calibri', 20, 'bold'))
-# Set geometry of the label
-model.place(relx=0.4, rely=0, relwidth=0.2, relheight=0.6)
-
-frame = tk.Frame(root, bg='#DDA0DD', bd=10)
-frame.place(relx=0.5, rely=0.2, relwidth=0.90, relheight=0.1, anchor='n')
-
-frame1 = tk.Frame(root, bg='#595959', bd=10)
-frame1.place(relx=0.5, rely=0.1, relwidth=0.90, relheight=0.1, anchor='n')
-
-# frame_a = tk.Frame(root, bg='#595959', bd=10)
-# frame_a.place(relx=0.08, rely=0.1, relwidth=0.1, relheight=0.7, anchor='n')
-
-# Definition of the open_file function
-
 file_name = "null"
-option = "null"
+option = "null" 
 
-def show(file_name):
-    print("show fun started ", file_name)
-    
+
+def show(file_name, title="Input Image"):
+    print("started show() \t", file_name)
+
     # Create a window
     window = tk.Toplevel()
-    window.title("Input Image")
+    window.title(title)
 
     img = cv2.imread(file_name)
     cv_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -70,7 +36,8 @@ def show(file_name):
     # Add a PhotoImage to the Canvas
     canvas.create_image(0, 0, image=photo, anchor=tk.NW)
     window.mainloop()
-    
+
+
 def open_file():
     print("open_file fun started\n")
 
@@ -80,96 +47,156 @@ def open_file():
 
     file_name = file
 
-    print('showImage \n')
-
     show(file_name)
-    
 
 
-def enhanceColorImage():
-    global file_name
-    print("start enhanceColorImage")
-    
+def enhanceImage():
+    global file_name, option
+
+    print("started enhanceImage() \n")
+    print(option, "\n")
+
+    if(Code.isGreyScale(file_name)):
+        new_file_name = Code.enhanceGreyscaleImage(file_name)
+        show(new_file_name)
+
+        return
+    if(option == "null"):
+        print("Please select one option\n")
+        return
+    elif(option == "traditional"):
+        print("if option == traditional\n")
+        new_file_name = Code.traditionalMethod(file_name)
+        show(new_file_name)
+        return
+
     new_file_name = Code.enhanceColorImage(file_name)
-    print("stop enhanceColorImage")
-    
-    print("showing ", new_file_name)
 
     show(new_file_name)
-    
+
+    print("stopped enhanceImage() \n")
+
+
 # functions for selecting options
 def selectTraditional():
     print("traditional selected")
 
+    global option
     option = "traditional"
-    
+
+
 def selectAdaptive():
     print("adaptive selected")
 
+    global option
     option = "adaptive"
-    
+
+
 def selectCLAHE():
     print("clahe selected")
 
+    global option
     option = "clahe"
 
-# vertical
 
-# blur = tk.Button(frame, text='Blur', command=lambda: Blur(path), font=('calibri', 20, 'bold'))
-# blur.place(relx=0.4, relheight=0.9, relwidth=0.2)
+def main():
+    # Create an instance of tkinter frame
+    root = tk.Tk()
 
-# Menu bar
+    # Give title to the tkinter frame
+    root.title('Image Enhancement')
 
-menu2 = tk.Button(frame1, text='Traditional', command=selectTraditional,
-                  bg="white", font=('calibri', 20, 'bold'))
-menu2.place(relx=0.3, rely=0, relheight=0.7, relwidth=0.2)
+    # Create canvas
+    canvas = tk.Canvas(root, bg='#DDA0DD', height=10000, width=10000)
+    canvas.pack()
 
-menu3 = tk.Button(frame1, text='Adaptive', command=selectAdaptive,
-                  bg="white", font=('calibri', 20, 'bold'))
-menu3.place(relx=0.5, rely=0, relheight=0.7, relwidth=0.2)
+    # background_image = tk.PhotoImage('Black')
+    # background_label = tk.Label(root, image=background_image)
+    # background_label.place(relwidth=1, relheight=1)
 
-menu4 = tk.Button(frame1, text='CLAHE', command=selectCLAHE,
-                  bg="white", font=('calibri', 20, 'bold'))
-menu4.place(relx=0.7, rely=0, relheight=0.7, relwidth=0.2)
+    # Create different frames into the canvas
+    # Our model
+    main_frame = tk.Frame(root, bg='#595959', bd=10)
+    # Set the position of frame
+    main_frame.place(relx=0.5, rely=0, relwidth=0.90,
+                     relheight=0.1, anchor='n')
+    # Give label for the frame
+    model = tk.Label(main_frame, text="Our Model",
+                     bg="white", font=('calibri', 20, 'bold'))
+    # Set geometry of the label
+    model.place(relx=0.4, rely=0, relwidth=0.2, relheight=0.6)
 
-# Create button to select the input image
-input_img = tk.Button(frame, text='Browse', command=open_file,
-                      bg="yellow", font=('calibri', 20, 'bold'))
-input_img.place(relx=0.2, rely=0.2, relheight=0.7, relwidth=0.2)
+    frame = tk.Frame(root, bg='#DDA0DD', bd=10)
+    frame.place(relx=0.5, rely=0.2, relwidth=0.90, relheight=0.1, anchor='n')
 
-# output_img = tk.Button(frame, text='Greyscale', command=open_file,bg="grey", font=('calibri', 20, 'bold'))
-# output_img.place(relx=0.4, relheight=0.9, relwidth=0.2)
+    frame1 = tk.Frame(root, bg='#595959', bd=10)
+    frame1.place(relx=0.5, rely=0.1, relwidth=0.90, relheight=0.1, anchor='n')
 
-# Create button to equalize the inputed image
-output_img1 = tk.Button(frame, text='Enhance', command=lambda: enhanceColorImage(
-), bg="yellow", font=('calibri', 20, 'bold'))
+    # frame_a = tk.Frame(root, bg='#595959', bd=10)
+    # frame_a.place(relx=0.08, rely=0.1, relwidth=0.1, relheight=0.7, anchor='n')
 
-output_img1.place(relx=0.6, rely=0.2, relheight=0.7, relwidth=0.2)
+    # Definition of the open_file function
 
-# Create lower frame for input image
-lower_frame = tk.Frame(root, bg='#595959', bd=10)
-# Set geometry for the frame
-lower_frame.place(relx=0.3, rely=0.3, relwidth=0.30, relheight=0.6, anchor='n')
-bg_color = 'white'
+    # vertical
 
-# Create lower frame for enhanced image
-lower_frame1 = tk.Frame(root, bg='#595959', bd=10)
-# Set Geometry of the frame
-lower_frame1.place(relx=0.7, rely=0.3, relwidth=0.30,
-                   relheight=0.6, anchor='n')
+    # blur = tk.Button(frame, text='Blur', command=lambda: Blur(path), font=('calibri', 20, 'bold'))
+    # blur.place(relx=0.4, relheight=0.9, relwidth=0.2)
 
-bg_color1 = 'white'
+    # Menu bar
+
+    menu2 = tk.Button(frame1, text='Traditional', command=selectTraditional,
+                      bg="white", font=('calibri', 20, 'bold'))
+    menu2.place(relx=0.3, rely=0, relheight=0.7, relwidth=0.2)
+
+    menu3 = tk.Button(frame1, text='Adaptive', command=selectAdaptive,
+                      bg="white", font=('calibri', 20, 'bold'))
+    menu3.place(relx=0.5, rely=0, relheight=0.7, relwidth=0.2)
+
+    menu4 = tk.Button(frame1, text='CLAHE', command=selectCLAHE,
+                      bg="white", font=('calibri', 20, 'bold'))
+    menu4.place(relx=0.7, rely=0, relheight=0.7, relwidth=0.2)
+
+    # Create button to select the input image
+    input_img = tk.Button(frame, text='Browse', command=open_file,
+                          bg="yellow", font=('calibri', 20, 'bold'))
+    input_img.place(relx=0.2, rely=0.2, relheight=0.7, relwidth=0.2)
+
+    # output_img = tk.Button(frame, text='Greyscale', command=open_file,bg="grey", font=('calibri', 20, 'bold'))
+    # output_img.place(relx=0.4, relheight=0.9, relwidth=0.2)
+
+    # Create button to equalize the inputed image
+    output_img1 = tk.Button(frame, text='Enhance', command=lambda: enhanceImage(
+    ), bg="yellow", font=('calibri', 20, 'bold'))
+
+    output_img1.place(relx=0.6, rely=0.2, relheight=0.7, relwidth=0.2)
+
+    # Create lower frame for input image
+    lower_frame = tk.Frame(root, bg='#595959', bd=10)
+    # Set geometry for the frame
+    lower_frame.place(relx=0.3, rely=0.3, relwidth=0.30,
+                      relheight=0.6, anchor='n')
+    bg_color = 'white'
+
+    # Create lower frame for enhanced image
+    lower_frame1 = tk.Frame(root, bg='#595959', bd=10)
+    # Set Geometry of the frame
+    lower_frame1.place(relx=0.7, rely=0.3, relwidth=0.30,
+                       relheight=0.6, anchor='n')
+
+    bg_color1 = 'white'
+
+    # Create canvas to display the enhanced image
+    output1 = tk.Canvas(lower_frame1, bg=bg_color1,
+                        bd=10, highlightthickness=0)
+    # Set the geometry of canvas
+    output1.place(relx=0, rely=0, relwidth=1, relheight=0.9)
+    # Give label to the output canvas
+    output_label = tk.Label(lower_frame1, text="Output image",
+                            bg="yellow", font=('calibri', 20, 'bold'))
+    output_label.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
+
+    # Start the tkinter frame
+    root.mainloop()
 
 
-# Create canvas to display the enhanced image
-output1 = tk.Canvas(lower_frame1, bg=bg_color1, bd=10, highlightthickness=0)
-# Set the geometry of canvas
-output1.place(relx=0, rely=0, relwidth=1, relheight=0.9)
-# Give label to the output canvas
-output_label = tk.Label(lower_frame1, text="Output image",
-                        bg="yellow", font=('calibri', 20, 'bold'))
-output_label.place(relx=0, rely=0.9, relwidth=1, relheight=0.1)
-
-# Start the tkinter frame 
-root.mainloop()
-
+main()
